06ea38dcde2e838fe57f0f1be924bee77516715b
Sergio Seva <sseva@traackr.com>
2022-03-19 12:28:48 -0300

    adding comments

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const { process_shopify } = require('./data-providers/shopify');
 const { process_woocomerce } = require('./data-providers/woocomerce');
 const { calculate_stats } = require('./processor/stats-calculator');
 
-
+//repository where our data providers processors will store the data
 let people = new Map();
 
+//gather the information from external systems
 process_woocomerce(people)
 
 process_shopify(people)
 
+//process the data as we want
 calculate_stats(people)
-
