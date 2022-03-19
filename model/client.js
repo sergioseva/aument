feaf3837418df0f22b2263dcc5263e4c6a400e0c
Sergio Seva <sseva@traackr.com>
2022-03-19 11:23:21 -0300

    initial commit

---
+++ model/client.js
@@ -0,0 +1,13 @@
+class Client {
+    constructor(email,name,sum_orders,quantity_orders) {
+      this.email=email
+      this.name=name
+      this.sum_orders=sum_orders;
+      this.quantity_orders=quantity_orders
+    }
+     total_average() {
+      return  this.sum_orders/this.quantity_orders
+    }
+};
+
+module.exports = {Client}
