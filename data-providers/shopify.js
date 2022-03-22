be892fdd1dc1d2218f51e93781729d6a2b22bf16
Sergio Seva <sseva@traackr.com>
2022-03-22 15:37:50 -0300

    fix calculating orders quantity and product

--- data-providers/shopify.js
+++ data-providers/shopify.js
@@ -3,15 +3,13 @@ const { Client } = require('../model/client');
 
 function process_shopify(people_map) {
     Shopify.forEach(v => {
-        let client_stats=people_map.get(v.customer.email);
-        v.products.forEach(p => {    
+        let client_stats=people_map.get(v.customer.email); 
             if (client_stats) {
-                    client_stats.quantity_orders+=p.quantity
-                    client_stats.sum_orders+=Number(p.price)*p.quantity
+                    client_stats.quantity_orders++
+                    client_stats.sum_orders+=Number(v.price)
             } else {
-                client_stats = new Client(v.customer.email,v.customer.name,Number(p.price)*p.quantity,p.quantity)
+                client_stats = new Client(v.customer.email,v.customer.name,Number(v.price),1)
             }
-        })
         people_map.set(v.customer.email,client_stats)
     } )
     return people_map
--- data-providers/woocomerce.js
+++ data-providers/woocomerce.js
@@ -3,15 +3,14 @@ const { Client } = require('../model/client');
 
 function process_woocomerce(people_map) {
     Woocommerce.forEach(v => {
-        let client_stats=people_map.get(v.customer.email);
-        v.products.forEach(p => {    
-            if (client_stats) {
-                    client_stats.quantity_orders+=p.quantity
-                    client_stats.sum_orders+=Number(p.money)*p.quantity
-            } else {
-                client_stats = new Client(v.customer.email,v.customer.name,Number(p.money)*p.quantity,p.quantity)
-            }
-        })
+        let client_stats=people_map.get(v.customer.email); 
+        if (client_stats) {
+                client_stats.quantity_orders++
+                client_stats.sum_orders+=Number(v.money)
+        } else {
+            client_stats = new Client(v.customer.email,v.customer.name,Number(v.money),1)
+        }
+    people_map.set(v.customer.email,client_stats)
         people_map.set(v.customer.email,client_stats)
     } )
 }
